aaaecc70f622afb2c90b6192293539eac74c2b47
soheldatta17 <soheldatta17@gmail.com>
2024-01-20 18:05:10 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import './Chatbot.css'; // Import your CSS file
 import Chatbot from './Chatbot';
 import Pic from './Pic';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
+inject();
+injectSpeedInsights();
 
 const App = () => {
   const [chat, setChat] = useState(true);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { injectSpeedInsights } from '@vercel/speed-insights';
-import { inject } from '@vercel/analytics';
-inject();
-injectSpeedInsights();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
